27bc0ccf441ca040755ce06d069e928d4fe28a1d
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-04-22 18:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	let newWrd = s.split(" ");
+	console.log(newWrd[0])
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
